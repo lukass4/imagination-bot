dbcde7d2506387919b9bba7893d726ce7cf0d484
Lukas <76885856+LJ-exe@users.noreply.github.com>
2021-04-12 20:01:46 +0100

    V1.1 Embed update
    BOT UPDATE! [v1.1], The embed update!
    [/]Changed all of main commands to embeds!
    [+]Added @The Imagination Test so when I am working on the bot the main one can stay on!

--- bot.py
+++ bot.py
@@ -11,7 +11,9 @@ import json
 import math
 from mojang import MojangAPI
 
-client = commands.Bot(command_prefix = "/")
+PREFIX = "/"
+
+client = commands.Bot(command_prefix = PREFIX)
 
 @client.event
 async def on_ready():
@@ -24,6 +26,7 @@ def is_bot_admin(ctx):
 
 @client.command()
 async def verify(ctx, user):
+    await ctx.channel.purge(limit=1)
     if not user == None:
        
         uuid = MojangAPI.get_uuid(user)
@@ -40,13 +43,11 @@ async def verify(ctx, user):
                         return i + float(xp - xps[i-1]) / (xps[i] - xps[i-1])
         net_exp = DATA["player"]["networkExp"]
         net_level = math.floor((((math.sqrt(net_exp + 15312.5)) - (125 / math.sqrt(2))) / 25*math.sqrt(2)) / 2)
-        await ctx.send(f"{user}'s Network level is {net_level}")
         bedwars_level = DATA["player"]["achievements"]["bedwars_level"]
         try:
             bedwars_wins = DATA["player"]["achievements"]["bedwars_wins"]
         except KeyError:
             bedwars_wins = 0
-        await ctx.send(f"{user}'s Bedwars level is {bedwars_level} with {bedwars_wins} wins.")
         skywars_exp = DATA["player"]["stats"]["SkyWars"]["skywars_experience"]
         skywars_level = math.floor(sw_xp_to_lvl(skywars_exp))
         try:
@@ -65,18 +66,43 @@ async def verify(ctx, user):
             skywars_wins = skywars_wins + skywars_wins_teams
         except KeyError:
             skywars_wins = skywars_wins
-        await ctx.send(f"{user}'s Skywars level is {skywars_level} with {skywars_wins} wins")
         if bedwars_level >= 75 or skywars_level >= 4:
             if net_level >= 25:
-                await ctx.send(f"{user} **is** eligible for the main guild.")
-            elif user == "hamza_talaat":
-                await ctx.send(f"{user} **is not** eligible for the main guild.")
+                eligible = "yes"
             else:
-                await ctx.send(f"{user} **is not** eligible for the main guild.")
+                eligible = "no"
         elif net_level >= 100:
-            await ctx.send(f"{user} **may be** eligible for the main guild. \n (as their network level is above 100 but they dont meet the SW or BW reqs)")
+            eligible = "maybe"
         else:
-            await ctx.send(f"{user} **is not** eligible for the main guild.")
+            eligible = "no"
+        
+        if eligible == "yes":
+            embedVar = discord.Embed(title=f"{user} Stats", description=f"Here are the stats for {user}", color=0x2fd668)
+            embedVar.add_field(name="Network", value=f"{user}'s Network level is {net_level}", inline=False)
+            embedVar.add_field(name="Bedwars", value=f"{user}'s Bedwars level is {bedwars_level} with {bedwars_wins} wins.", inline=False)
+            embedVar.add_field(name="Skywars", value=f"{user}'s Skywars level is {skywars_level} with {skywars_wins} wins", inline=False)
+            embedVar.add_field(name="Eligible", value=f"{user} **is** eligible for the main guild.", inline=False)
+            await ctx.send(embed=embedVar)
+        elif eligible == "no":
+            embedVar = discord.Embed(title=f"{user} Stats", description=f"Here are the stats for {user}", color=0x2fd668)
+            embedVar.add_field(name="Network", value=f"{user}'s Network level is {net_level}", inline=False)
+            embedVar.add_field(name="Bedwars", value=f"{user}'s Bedwars level is {bedwars_level} with {bedwars_wins} wins.", inline=False)
+            embedVar.add_field(name="Skywars", value=f"{user}'s Skywars level is {skywars_level} with {skywars_wins} wins", inline=False)
+            embedVar.add_field(name="Eligible", value=f"{user} **is not** eligible for the main guild.", inline=False)
+            await ctx.send(embed=embedVar)
+        elif eligible == "maybe":
+            embedVar = discord.Embed(title=f"{user} Stats", description=f"Here are the stats for {user}", color=0x2fd668)
+            embedVar.add_field(name="Network", value=f"{user}'s Network level is {net_level}", inline=False)
+            embedVar.add_field(name="Bedwars", value=f"{user}'s Bedwars level is {bedwars_level} with {bedwars_wins} wins.", inline=False)
+            embedVar.add_field(name="Skywars", value=f"{user}'s Skywars level is {skywars_level} with {skywars_wins} wins", inline=False)
+            embedVar.add_field(name="Eligible", value=f"{user} **may be** eligible for the main guild.", inline=False)
+            await ctx.send(embed=embedVar)
+        else:
+            embedVar = discord.Embed(title=f"{user} Stats", description=f"Here are the stats for {user}", color=0x2fd668)
+            embedVar.add_field(name="Network", value=f"{user}'s Network level is {net_level}", inline=False)
+            embedVar.add_field(name="Bedwars", value=f"{user}'s Bedwars level is {bedwars_level} with {bedwars_wins} wins.", inline=False)
+            embedVar.add_field(name="Skywars", value=f"{user}'s Skywars level is {skywars_level} with {skywars_wins} wins", inline=False)
+            embedVar.add_field(name="Eligible", value=f"{user} **is not** eligible for the main guild.", inline=False)
 
 @verify.error
 async def verify_error(ctx, error):
@@ -85,6 +111,7 @@ async def verify_error(ctx, error):
 
 @client.command()
 async def qverify(ctx, user):
+    await ctx.channel.purge(limit=1)
     def sw_xp_to_lvl(xp):
         xps = [0, 20, 70, 150, 250, 500, 1000, 2000, 3500, 6000, 10000, 15000]
         if xp >= 15000:
@@ -105,13 +132,17 @@ async def qverify(ctx, user):
 
     if bedwars_level >= 75 or skywars_level >= 4:
         if net_level >= 25:
-            await ctx.send(f"{user} **is** eligible for the main guild.")
+            embedVar = discord.Embed(title=f"{user} **is** eligible for the main guild.", description=f"{user} **is** eligible for the main guild.", color=0x2fd668)
+            await ctx.send(embed=embedVar)
         else:
-            await ctx.send(f"{user} **is not** eligible for the main guild.")
+            embedVar = discord.Embed(title=f"Is {user} eligible?", description=f"{user} **is not** eligible for the main guild.", color=0x2fd668)
+            await ctx.send(embed=embedVar)
     elif net_level >= 100:
-        await ctx.send(f"{user} **may be** eligible for the main guild. \n (as their network level is above 100 but they dont meet the SW or BW reqs)")
+        embedVar = discord.Embed(title=f"Is {user} eligible?", description=f"{user} **may be** eligible for the main guild. \n (as their network level is above 100 but they dont meet the SW or BW reqs)", color=0x2fd668)
+        await ctx.send(embed=embedVar)
     else:
-        await ctx.send(f"{user} **is not** eligible for the main guild.")
+        embedVar = discord.Embed(title=f"Is {user} eligible?", description=f"{user} **is not** eligible for the main guild.", color=0x2fd668)
+        await ctx.send(embed=embedVar)
 
 @qverify.error
 async def qverify_error(ctx, error):
@@ -129,6 +160,7 @@ async def uuid_error(ctx, error):
 
 @client.command()
 async def bedwars(ctx, user):
+    await ctx.channel.purge(limit=1)
     uuid = MojangAPI.get_uuid(user)
     url = f"https://api.hypixel.net/player?key=924fecf1-37f2-421a-ae07-7f3ca74d9790&uuid={uuid}"
     DATA = requests.get(url).json()
@@ -166,14 +198,23 @@ async def bedwars(ctx, user):
         bedwars_wins = DATA["player"]["stats"]["Bedwars"]["wins_bedwars"]
     except KeyError:
         bedwars_wins = 0
-    await ctx.send(f"""{user}'s Bedwars Stats:
-Level: `{bedwars_level}`
-Wins: `{bedwars_wins}` 
-Final Kills: `{final_kills}`
-Kills: `{bedwars_kills}`
-Winstreak: `{winstreak}`
-Losses: `{bedwars_losses}`
-Final Deaths: `{bedwars_final_deaths}`
-Deaths: `{bedwars_deaths}`
-Plancke: <https://plancke.io/hypixel/player/stats/{user}#BedWars>""")
+    embedVar = discord.Embed(title=f"{user}'s bedwars stats", description=f"This is {user}'s bedwars stats", color=0x2fd668)    
+    embedVar.add_field(name="Level", value=f"{bedwars_level}", inline=False)
+    embedVar.add_field(name="Wins", value=f"{bedwars_wins}", inline=False)
+    embedVar.add_field(name="Final Kills", value=f"{final_kills}", inline=False)
+    embedVar.add_field(name="Kills", value=f"{bedwars_kills}", inline=False)
+    embedVar.add_field(name="Winstreak", value=f"{winstreak}", inline=False)
+    embedVar.add_field(name="Losses", value=f"{bedwars_losses}", inline=False)
+    embedVar.add_field(name="Final Deaths", value=f"{bedwars_final_deaths}", inline=False)
+    embedVar.add_field(name="Deaths", value=f"{bedwars_level}", inline=False)
+    embedVar.add_field(name="Plancke", value=f"<https://plancke.io/hypixel/player/stats/{user}#BedWars>", inline=False)
+    await ctx.send(embed=embedVar)
+
+@bedwars.error
+async def bedwars_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("SPECIFY A **PLAYER** :angry:")
+
+
+
 client.run("ODMwMTIwNjc5NDc5MTgxMzEz.YHCEIA.kBkWUQlLUV4kfQ5MVTBkKD7xAag")
